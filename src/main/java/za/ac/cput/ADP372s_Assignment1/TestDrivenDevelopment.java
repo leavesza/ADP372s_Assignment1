c50e91c9d0e9cee417e8790d82176ab81812dd6a
MARVIN <marvinlamohr@outlook.com>
2021-03-25 19:03:55 +0200

    Create Project File

---
+++ src/main/java/za/ac/cput/ADP372s_Assignment1/TestDrivenDevelopment.java
@@ -0,0 +1,4 @@
+package za.ac.cput.ADP372s_Assignment1;
+
+public class TestDrivenDevelopment {
+}
---
+++ src/test/java/za/ac/cput/ADP372s_Assignment1/TestDrivenDevelopmentTest.java
@@ -0,0 +1,7 @@
+package za.ac.cput.ADP372s_Assignment1;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TestDrivenDevelopmentTest {
+
+}
