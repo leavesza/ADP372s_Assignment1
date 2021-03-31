fbef201931a21f8ebdac346d8377d268b0f39121
Leaves <yaseenlevy21@gmail.com>
2021-03-31 14:20:14 +0200

    Initial Commit

---
+++ src/main/java/za/ac/cput/ADP372s_Assignment1/Car.java
@@ -0,0 +1,36 @@
+package za.ac.cput.ADP372s_Assignment1;
+
+public class Car {
+
+    private int speed;
+    private String model;
+    private boolean fast = false;
+
+    public Car(String model,int speed){
+
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public boolean isFast() {
+        return fast;
+    }
+
+    public void modifyCar(){
+        fast = true;
+    }
+}
---
+++ src/test/java/za/ac/cput/ADP372s_Assignment1/CarTest.java
@@ -0,0 +1,51 @@
+package za.ac.cput.ADP372s_Assignment1;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CarTest {
+
+
+    private Car car1 = new Car("Ford",200);
+    private Car car2 = new Car("Ford",200);
+    //Object equality
+    @Test
+    public void testGetModel() {
+
+        assertEquals(car1.getModel(),car2.getModel());
+    }
+
+    //Object Identity
+    @Test
+    public void testCar(){
+        Car car1;
+        Car car2;
+        car1 = new Car("Ford",200);
+        car2 = car1;
+        assertSame(car1,car2);
+    }
+
+    //Failing test
+    @Test
+    public void testFailGetModel() {
+        assertNotEquals("Porche",car1.getModel());
+    }
+
+
+    @Timeout(10)
+    @Test
+    public void testUnhappyToStart(){
+        assertFalse(car1.isFast());
+    }
+
+
+    @Disabled
+    @Test
+    public void testHappyAfterPlay(){
+        car1.isFast();
+        assertTrue(car1.isFast());
+    }
+}
